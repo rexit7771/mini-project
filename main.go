4831be0b2c928f4fc747f1dbb620ab39e0cfd316
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2024-12-20 17:41:04 +0700

    Update on .env

--- main.go
+++ main.go
@@ -23,11 +23,11 @@ func main() {
 	}
 	psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s
 	password=%s dbname=%s sslmode=disable`,
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
 	)
 
 	DB, err = sql.Open("postgres", psqlInfo)
@@ -49,5 +49,5 @@ func main() {
 	router.PUT("/persons/:id", controllers.UpdatePerson)
 	router.DELETE("/persons/:id", controllers.DeletePerson)
 
-	router.Run(":8080")
+	router.Run(":" + os.Getenv("PORT"))
 }
